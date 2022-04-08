5d8f59b2bcfd77944b229abb09906de00c579eb7
romain2518 <rolenoir25@gmail.com>
2022-04-08 22:07:59 +0200

    Swipes can now be used to move the player

--- maze/javascript/game.js
+++ maze/javascript/game.js
@@ -1,3 +1,32 @@
+/** Game module tree :
+ *
+ * Game :
+ * |- init()
+ * |- cases {}
+ * |- loadLevel(level)
+ * |- keydownHandler(event)
+ * |- checkCase()
+ * |- move
+ *      |- up()
+ *      |- right()
+ *      |- down()
+ *      |- left()
+ * |- coordinates
+ *      |- x
+ *      |- y
+ *      |- toggleDisplay()
+ * |- map
+ * |- touchstartHandler(event)
+ * |- touchendHandler(event)
+ * |- swipeVars
+ *      |- startX       |- startY
+ *      |- distX        |- distY
+ *      |- startTime    |- elapsedTime
+ *      |- minDist              //! Params     
+ *      |- maxPerpDistAllowed   //! Params
+ *      |- allowedTime          //! Params
+ */
+
 const game = {
     init: function () {
         game.loadLevel(0);
@@ -20,6 +49,10 @@ const game = {
 
         //Keydown event
         document.addEventListener('keydown', game.keydownHandler);
+
+        //Swipe events
+        document.body.addEventListener('touchstart', game.touchstartHandler);
+        document.body.addEventListener('touchend', game.touchendHandler);
     },
     cases: {
         'x': 'wall',
@@ -62,6 +95,8 @@ const game = {
             mazeElm.appendChild(lineElm);
             y++;
         });
+
+        console.log('Level loaded');
     },
     keydownHandler: function (event) {
         if (event.repeat) return;
@@ -83,11 +118,11 @@ const game = {
                 game.move.left();
                 break;
         }
-        game.checkCase();
     },
     checkCase: function () {
         if (document.querySelector(`section div:nth-child(${game.coordinates.y+1}) div:nth-child(${game.coordinates.x+1})`).classList.contains('house')) {
             window.alert('Victoire !!!');
+            console.log('Win');
         }
     },
     move: {
@@ -98,6 +133,7 @@ const game = {
             game.coordinates.toggleDisplay();
             game.coordinates.y--;
             game.coordinates.toggleDisplay();
+            game.checkCase();
         },
         right: function () {
             if (game.coordinates.x >= game.map[0].length-1) return;
@@ -106,6 +142,7 @@ const game = {
             game.coordinates.toggleDisplay();
             game.coordinates.x++;
             game.coordinates.toggleDisplay();
+
         },
         down: function () {
             if (game.coordinates.y >= game.map.length-1) return;
@@ -114,6 +151,7 @@ const game = {
             game.coordinates.toggleDisplay();
             game.coordinates.y++;
             game.coordinates.toggleDisplay();
+
         },
         left: function () {
             if (game.coordinates.x <= 0) return;
@@ -122,6 +160,7 @@ const game = {
             game.coordinates.toggleDisplay();
             game.coordinates.x--;
             game.coordinates.toggleDisplay();
+
         },
     },
     coordinates: {
@@ -132,4 +171,44 @@ const game = {
         },
     },
     map: null,
+    touchstartHandler: function (event) {
+        game.swipeVars.startX = event.changedTouches[0].pageX;
+        game.swipeVars.startY = event.changedTouches[0].pageY;
+
+        game.swipeVars.startTime = new Date().getTime(); //Record time when finger first makes contact with surface
+    },
+    touchendHandler: function (event) {
+        game.swipeVars.distX = event.changedTouches[0].pageX - game.swipeVars.startX;
+        game.swipeVars.distY = event.changedTouches[0].pageY - game.swipeVars.startY;
+
+        game.swipeVars.elapsedTime = new Date().getTime() - game.swipeVars.startTime;
+
+        if (game.swipeVars.elapsedTime <= game.swipeVars.allowedTime) {
+            if (Math.abs(game.swipeVars.distX) >= game.swipeVars.minDist && Math.abs(game.swipeVars.distY) <= game.swipeVars.maxPerpDistAllowed) { //Horizontal swipe
+                if (game.swipeVars.distX < 0) { //Left swipe detected
+                    game.move.left();
+                } else { //Right swipe detected
+                    game.move.right();
+                }
+            } else if (Math.abs(game.swipeVars.distY) >= game.swipeVars.minDist && Math.abs(game.swipeVars.distX) <= game.swipeVars.maxPerpDistAllowed) { //Vertical swipe
+                if (game.swipeVars.distY < 0) { //Up swipe detected
+                    game.move.up();
+                } else { //Down swipe detected
+                    game.move.down();
+                }
+            }
+        }
+    },
+    swipeVars: {
+        startX: null,
+        startY: null,
+        distX: null,
+        distY: null,
+        startTime: null,
+        elapsedTime: null,
+        //! Parameters for touches to be considered as swipes
+        minDist: 150, //Required min distance traveled to be considered swipe
+        maxPerpDistAllowed: 100, //Maximum distance allowed at the same time in perpendicular direction
+        allowedTime: 300, //Maximum time allowed to travel the distance
+    },
 };
